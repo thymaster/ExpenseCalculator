5e2e4a4139ba6345523130a8b332793af4ef6e97
Anish Sarkar <anish_pg@ece.nits.ac.in>
2021-06-24 13:20:54 +0530

    items recieved dynamically using props

--- src/Components/Expenses.jsx
+++ src/Components/Expenses.jsx
@@ -5,10 +5,10 @@ export function Expenses(props) {
   return (
     <div>
       <div className="expenseItem">
-        <div>March 28th 2021</div>
+        <div> {props.date.toISOString()} </div>
         <div className="expenseItem__description">
-          <h2>Car Insurance</h2>
-          <div className="expenseItem__price">$294.67</div>
+          <h2> {props.title} </h2>
+          <div className="expenseItem__price"> ${props.amount} </div>
         </div>
       </div>
     </div>
